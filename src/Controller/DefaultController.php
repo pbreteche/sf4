b137cf1bb73c4f77e40d0af1aa2094330cd935a7
Pierre Bretéché <pbreteche@dawan.fr>
2018-11-13 10:30:59 +0100

    request response

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Controller;
+
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class DefaultController
+{
+
+
+    public function index(Request $request): Response
+    {
+        $name = $request->query->get('nom', 'tout le monde'); // $_GET
+
+        $formData = $request->request->get('nom-du-champs');// $_POST
+
+        $uri = $request->server->get('REQUEST_URI');
+
+        $response = new Response();
+
+        $response->setContent('<h1>Hello ' . $name . '!</h1>');
+
+        $response->setStatusCode(Response::HTTP_PARTIAL_CONTENT);
+
+        $response->headers->set('Content-Type', 'text/html');
+        $response->headers->set('X-Formation', 'contenu perso');
+
+        return $response;
+    }
+}
