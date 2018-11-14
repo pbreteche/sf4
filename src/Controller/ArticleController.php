4c34495a96cdfddbc5750fcfc3ed4d9dc04cbc94
Pierre Bretéché <pbreteche@dawan.fr>
2018-11-14 12:31:57 +0100

    formulaire

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -4,7 +4,10 @@ namespace App\Controller;
 
 use App\Entity\Article;
 use App\Repository\ArticleRepository;
+use Doctrine\Common\Persistence\ObjectManager;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ArticleController extends AbstractController
@@ -32,6 +35,38 @@ class ArticleController extends AbstractController
     {
         return $this->render('article/detail.html.twig', [
             'article' => $article,
+        ]);
+    }
+
+    /**
+     * @Route("/article/new")
+     */
+    public function create(Request $request, ObjectManager $manager)
+    {
+        $article = new Article();
+        $form = $this->createFormBuilder($article, ['method' => 'POST'])
+            ->add('title')
+            ->add('content')
+            ->getForm();
+        $form->add('publish', SubmitType::class, [
+            'label' => 'Publier'
+        ]);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $article->setPublishedAt(new \DateTime());
+            $manager->persist($article);
+            $manager->flush();
+
+            $this->addFlash('success', 'Bravo, ça a bien marché!');
+            return $this->redirectToRoute('app_article_detail', [
+                'id' => $article->getId()
+            ]);
+        }
+
+        return $this->render('article/create.html.twig', [
+            'createForm' => $form->createView()
         ]);
     }
 }
